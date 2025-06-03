0a28e6ec3ba685f1c8c64d4ddcd6992b84eecef2
Jakub Kowieski <kubakowieski5151@gmail.com>
2025-06-04 00:41:11 +0200

    jk: minor logs

--- app/main.py
+++ app/main.py
@@ -88,6 +88,7 @@ async def proxy(path: str, request: Request, user: UserDep):
         )
 
     except Exception as exc:
+        print(exc)
         return JSONResponse(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             content={"error": "Internal Server Error", "details": str(exc)},
